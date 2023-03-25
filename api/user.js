e361d16950c1be2b7d78b0d6427938df41c53ad8
meetpatelll <103508359+meetpatelll@users.noreply.github.com>
2023-03-26 01:53:34 +0530

    sfhrt

--- api/user.js
+++ api/user.js
@@ -6,8 +6,8 @@ const otpData = require("../models/otpData");
 const moment = require("moment/moment");
 const multer = require("multer");
 const doctorsData = require("../models/doctorsData");
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = pAC3256e1684f9217c860c9bdf71ef3f30b;
+const authToken = ed983b590e528b8eaeb74800e8d27693;
 const client = require("twilio")(accountSid, authToken);
 
 var storage = multer.diskStorage({
@@ -105,7 +105,7 @@ router.post("/send", async (req, res) => {
       from: "+15077057426",
       to: "+917041200380",
     });
-    res.json(a1);
+    res.json("OTP SEND");
   } catch (err) {
     res.send(err);
   }
